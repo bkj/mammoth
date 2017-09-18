d55ac7b6a5e7ec204627f27dcc44a1ba6d13019e
bjohnson <ben@gophronesis.com>
2017-09-18 12:28:52 -0400

    adding hadam.py

---
+++ hadam.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+"""
+    hadam.py
+    
+    ADAM-based hyperoptimizer
+    
+    !! Not actually different than regular ADAM, other than
+        a) how it handles shapes of parameteres
+        b) gradients get passed in manually, rather than w/ autograd
+"""
+
+class HADAM(object):
+    def __init__(self, params, step_size=0.1, b1=0.1, b2=0.01, eps=10**-4, lam=10**-4):
+        
+        self._flat_params = torch.cat([p.contiguous().view(-1) for p in params])
+        
+        self._sizes = [p.size() for p in params]
+        self._numel = sum([p.numel() for p in params])
+        
+        self.step_size = step_size
+        self.b1        = b1
+        self.b2        = b2
+        self.eps       = eps
+        self.lam       = lam
+        self.m         = self._flat_params.clone().zero_()
+        self.v         = self._flat_params.clone().zero_()
+        self.iter      = 0
+    
+    @property
+    def params(self):
+        offset = 0
+        for size in self._sizes:
+            numel = np.prod(size)
+            yield self._flat_params[offset:offset + numel].view(size)
+            offset += numel
+        
+        assert offset == self._numel, 'HyperADAM: offset != self._numel'
+        
+    def step_w_grads(self, grads):
+        g = torch.cat([grad.contiguous().view(-1) for grad in grads])
+        
+        b1t = 1 - (1 - self.b1) * (self.lam ** self.iter)
+        self.m = b1t * g + (1 - b1t) * self.m
+        self.v = self.b2 * (g ** 2) + (1 - self.b2) * self.v
+        mhat = self.m / (1 - (1 - self.b1) ** (self.iter + 1))
+        vhat = self.v / (1 - (1 - self.b2) ** (self.iter + 1))
+        
+        self.iter += 1
+        self._flat_params -= self.step_size * mhat / (vhat.sqrt() + self.eps)
+        return self.params
--- run-2.py
+++ run-2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 """
-    run.py
+    run-2.py
 """
 
 import sys
@@ -11,6 +11,7 @@ sys.path.append('/home/bjohnson/software/hypergrad')
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
+from collections import defaultdict
 
 import torch
 from torch import nn
@@ -25,6 +26,7 @@ from matplotlib import pyplot as plt
 
 from helpers import to_numpy
 from hsgd import HSGD
+from hadam import HADAM
 
 np.random.seed(123)
 _ = torch.manual_seed(456)
@@ -55,16 +57,6 @@ logit = lambda x: 1 / (1 + (-x).exp())
 d_logit = lambda x: x.exp() / ((1 + x.exp()) ** 2) # derivative of logit
 d_exp = lambda x: x.exp() # derivative of exponent
 
-def set_net_weights(net, val):
-    offset = 0
-    for p in net.parameters():
-        numel = p.numel()
-        if len(p.size()) == 1:
-            p.data.set_(val[offset:offset + numel].view_as(p.data))
-        else:
-            p.data.set_(val[offset:offset + numel].view_as(p.data.t()).t()) # !! autograd ordering
-        offset += numel
-    print offset
 
 def make_net(weight_scale=np.exp(-3), layers=[50, 50, 50]):
     
@@ -96,7 +88,7 @@ def deterministic_batch(X, y, sgd_iter, meta_iter, seed=0, batch_size=batch_size
 
 
 def train(net, opt, num_iters, meta_iter, seed=0):
-    train_hist, val_hist = [], []
+    hist = defaultdict([])
     for i in tqdm(range(num_iters)):
         X, y = deterministic_batch(X_train, y_train, sgd_iter=i, meta_iter=meta_iter, seed=0)
         
@@ -108,12 +100,12 @@ def train(net, opt, num_iters, meta_iter, seed=0):
         opt.step(i) if isinstance(opt, HSGD) else opt.step()
         
         train_acc = (to_numpy(net(X_train)).argmax(1) == to_numpy(y_train)).mean()
-        train_hist.append(train_acc)
-        
         val_acc = (to_numpy(net(X_val)).argmax(1) == y_val).mean()
-        val_hist.append(val_acc)
+        
+        hist['train'].append(train_acc)
+        hist['val'].append(val_acc)
     
-    return opt, val_hist, train_hist
+    return opt, hist
 
 
 def untrain(net, opt, num_iters, meta_iter, seed=0):
@@ -129,24 +121,13 @@ def untrain(net, opt, num_iters, meta_iter, seed=0):
 
 
 def do_meta_iter(meta_iter, net, lrs, mos):
-    opt = HSGD(
-        params=net.parameters(),
-        lrs=lrs.exp(),
-        mos=logit(mos),
-    )
-    
-    # opt = torch.optim.SGD(
-    #     net.parameters(),
-    #     lr=0.1,
-    #     momentum=0.5,
-    # )
+    opt = HSGD(params=net.parameters(), lrs=lrs, mos=mos)
     
     orig_weights = to_numpy(opt._get_flat_params())
     
     # Train
-    opt, val_hist, train_hist = train(net, opt, num_iters=num_iters, meta_iter=meta_iter, seed=0)
-    trained_weights = to_numpy(opt._get_flat_params())
-    print {"train_acc" : train_hist[-1], "val_acc" : val_hist[-1]}
+    opt, hist = train(net, opt, num_iters=num_iters, meta_iter=meta_iter, seed=0)
+    print {"train_acc" : hist['train'][-1], "val_acc" : hist['val'][-1]}
     
     # Init untrain
     def lf_all():
@@ -156,59 +137,32 @@ def do_meta_iter(meta_iter, net, lrs, mos):
     
     # Untrain
     opt = untrain(net, opt, num_iters, meta_iter)
+    
+    # Make sure SGD was reversed exactly
     untrained_weights = to_numpy(opt._get_flat_params())
     assert np.all(orig_weights == untrained_weights), 'meta_iter: orig_weights != untrained_weights'
     
-    return opt, val_hist, train_hist
+    return opt, hist
 
 # --
 # Run
 
-
+meta_iters = 50
 lrs = torch.DoubleTensor(np.full((num_iters, 8), -1.0)).cuda()
 mos = torch.DoubleTensor(np.full((num_iters, 8), 0.0)).cuda()
 
-meta_iters = 50
+hyperopt = HyperADAM([lrs, mos], step_size=0.04)
 
-b1 = 0.1
-b2 = 0.01
-eps = 10 ** -4
-lam = 10 ** -4
-step_size = 0.04
-
-m = [torch.zeros(lrs.size()).double().cuda(), torch.zeros(mos.size()).double().cuda()]
-v = [torch.zeros(lrs.size()).double().cuda(), torch.zeros(mos.size()).double().cuda()]
-
-all_val_hists, all_train_hists = [], []
+all_hist = defaultdict(list)
 for meta_iter in range(meta_iters):
-    print "\nmeta_iter=%d" % meta_iter
     
     net = make_net(weight_scale=np.exp(-3), layers=[50, 50, 50]).cuda()
-    opt, val_hist, train_hist = do_meta_iter(meta_iter, net, lrs, mos)
-    all_train_hists.append(train_hist)
-    all_val_hists.append(val_hist)
+    opt, hist = do_meta_iter(meta_iter, net, lrs.exp(), logit(mos))    
     
-    # ADAM step -- need to apply to all hypergrads
-    b1t = 1 - (1 - b1) * (lam ** meta_iter)
+    lrs, mos = hyperopt.step_w_grads([
+        opt.d_lrs * d_exp(lrs),
+        opt.d_mos * d_logit(mos)
+    ])
     
-    g = opt.d_lrs * d_exp(lrs) # !!
-    m[0] = b1t * g + (1-b1t) * m[0]
-    v[0] = b2 * (g ** 2) + (1 - b2) * v[0]
-    mhat = m[0] / (1 - (1 - b1) ** (meta_iter + 1))
-    vhat = v[0] / (1 - (1 - b2) ** (meta_iter + 1))
-    lrs -= step_size * mhat / (vhat.sqrt() + eps)
-    
-    g = opt.d_mos * d_logit(mos) # !!
-    m[1] = b1t * g + (1-b1t) * m[1]
-    v[1] = b2 * (g ** 2) + (1 - b2) * v[1]
-    mhat = m[1] / (1 - (1 - b1) ** (meta_iter + 1))
-    vhat = v[1] / (1 - (1 - b2) ** (meta_iter + 1))
-    mos -= step_size * mhat / (vhat.sqrt() + eps)
-
-
-cm = np.linspace(0, 1, len(all_val_hists))
-for i, v in enumerate(all_val_hists):
-    _ = plt.plot(v, c=plt.cm.rainbow(cm[i]), alpha=0.25)
-
-show_plot()
-
+    all_hist['train'].append(hist['train'])
+    all_hist['val'].append(hist['val'])
