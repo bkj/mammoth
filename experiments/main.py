8a86309e07475d90050e8980f2ff2f55f4a92750
bjohnson <bkj.322@gmail.com>
2018-05-03 22:32:10 -0400

    updating

--- experiments/main.py
+++ experiments/main.py
@@ -7,22 +7,26 @@
     on a toy MNIST network
 """
 
-import sys
-sys.path.append('/home/bjohnson/software/hypergrad')
-from hypergrad.data import load_data
-# ^^ Should remove this dependency at some point, but whatever for now
+from __future__ import print_function
 
+import sys
+import json
 import numpy as np
 from collections import defaultdict
 
 import torch
-from torch import nn, autograd
+from torch import nn
 from torch.nn import functional as F
 from torch.autograd import Variable
 
 from rsub import *
 from matplotlib import pyplot as plt
 
+sys.path.append('/home/bjohnson/software/hypergrad')
+from hypergrad.data import load_data
+# ^^ Should remove this dependency at some point, but whatever for now
+
+sys.path.append('.')
 from helpers import to_numpy, set_seeds
 from hyperlayer import HyperLayer
 
@@ -30,10 +34,8 @@ set_seeds(123)
 torch.backends.cudnn.benchmark = True
 torch.backends.cudnn.deterministic = True
 
-
 # --
 # IO
-
 
 X_train, y_train, X_val, y_val, _ = load_data(normalize=True)
 
@@ -42,6 +44,7 @@ y_train = Variable(torch.LongTensor(y_train.argmax(axis=1))).cuda()
 
 X_val = Variable(torch.FloatTensor(X_val)).cuda()
 y_val = Variable(torch.LongTensor(y_val.argmax(axis=1))).cuda()
+
 
 # --
 # Helpers
@@ -67,14 +70,14 @@ def make_net(weight_scale=np.exp(-3), layers=[50, 50, 50]):
 # --
 # Parameters
 
-num_iters = 200
+num_iters  = 200
 batch_size = 200
 
-hyper_lr = 0.01
-init_lr = 0.30
-init_mo = 0.50
-fix_init = True
-fix_data = False
+hyper_lr   = 0.01
+init_lr    = 0.30
+init_mo    = 0.50
+fix_init   = True
+fix_data   = False
 meta_iters = 250
 
 # --
@@ -99,13 +102,17 @@ hopt = torch.optim.Adam([lr_mean, lr_res, mo_mean, mo_res], lr=hyper_lr)
 set_seeds(123)
 hist = defaultdict(list)
 for meta_iter in range(0, meta_iters):
-    print 'meta_iter=%d' % meta_iter
+    print('meta_iter=%d' % meta_iter, file=sys.stderr)
     
+    # --
     # Transform hyperparameters
+    
     lrs = torch.clamp(lr_mean + lr_res, 0.001, 10.0)
     mos = torch.clamp(mo_mean + mo_res, 0.001, 0.999)
     
-    # Do hyperstep
+    # --
+    # Hyperstep
+    
     hopt.zero_grad()
     if fix_init:
         set_seeds(123)
@@ -115,19 +122,20 @@ for meta_iter in range(0, meta_iters):
     h(net, lrs, mos, val_data=(X_val, y_val))
     hopt.step()
     
-    # Log
-    print 'print val_acc=%f | loss_hist.tail.mean=%f | acc_hist.tail.mean=%f' % (
-        h.val_acc,
-        h.loss_hist[-10:].mean(),
-        h.acc_hist[-10:].mean(),
-    )
+    # --
+    # Logging
     
     hist['val_acc'].append(h.val_acc)
     hist['loss_hist'].append(h.loss_hist)
     hist['acc_hist'].append(h.acc_hist)
     hist['lrs'].append(to_numpy(lrs))
     hist['mos'].append(to_numpy(mos))
-
+    
+    print(json.dumps({
+        "val_acc"        : float(h.val_acc),
+        "tail_loss_mean" : float(h.loss_hist[-10:].mean()),
+        "tail_acc_mean"  : float(h.acc_hist[-10:].mean()),
+    }))
 
 
 _ = plt.plot([h[-1] for h in hist['acc_hist']])
---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# run.sh
+
+python experiments/main.py
